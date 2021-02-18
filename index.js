c9503d4279cf6f99d0cad1a2ccd8e73a02170e7c
Jonathan Terrell <terrell.jm@gmail.com>
2021-02-18 11:24:07 +0100

    Release v0.1.58

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @author Jonathan Terrell <jonathan.terrell@springbrook.es>
  * @copyright Copyright (c) 2019-2021 Springbrook S.L.
- * @license "Apache-2.0 with Commons Clause"
+ * @license "MIT"
  */
 
  module.exports = {
